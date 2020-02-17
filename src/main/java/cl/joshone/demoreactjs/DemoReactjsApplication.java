8f92faf97d59554d47fcc2df90d66ace61638093
joshone <jsalinasmagallanes@gmail.com>
2020-02-17 01:46:32 -0300

    java

---
+++ src/main/java/cl/joshone/demoreactjs/DemoReactjsApplication.java
@@ -0,0 +1,13 @@
+package cl.joshone.demoreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/joshone/demoreactjs/DemoReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.joshone.demoreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
